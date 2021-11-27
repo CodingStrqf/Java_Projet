d3cea412ede252b7725c19165cc03c8138e3d656
Xekerr <corentin.pimentel@gmail.com>
2021-11-27 09:23:21 +0100

    ajout de classe

--- src/model/Personnage.java
+++ src/model/Personnage.java
@@ -1,9 +1,46 @@
 package model;
 
-public class Personnage {
-    String nom;
+public abstract class Personnage {
 
-    private Personnage(String nom){
-        this.nom=nom;
+    private String pseudo;
+    private int vie;
+    private int def;
+    private int attaque;
+    private String type;
+    private String description;
+
+    public Personnage(String pseudo, int vie, int def, int attaque, String type, String description) {
+        this.pseudo = pseudo;
+        this.vie = vie;
+        this.def = def;
+        this.attaque = attaque;
+        this.type = type;
+        this.description = description;
     }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public int getVie() {
+        return vie;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public int getAttaque() {
+        return attaque;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
 }
